dbd46bc1fd62689cee1842de9fdd50f1c9a60c61
kaijunna <shell1016221@gmail.com>
2019-11-01 05:41:34 +0900

    no message

--- login-input.php
+++ login-input.php
@@ -1,2 +1,8 @@
-<a></a>
-<a></a>
+<?php require 'header.php';?>
+<?php require 'menu.php';?>
+<form action="login-output.php" method="post">
+ログイン名<input tupe="text" name="login"><br>
+パスワード<input tupe="password" name="password"><br>
+<input type="submit" value="ログイン">
+</form>
+<?php require 'footer.php';?>
